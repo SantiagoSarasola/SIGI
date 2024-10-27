97e76f0a5304e5c769fd350a874afa6277c38fb1
Luis Depetris <depetrisluis@gmail.com>
2024-10-27 18:19:10 -0300

    First Commit

---
+++ Back/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = 3000;
+
+// Interpretar JSON en el body.
+app.use(express.json());
+
+// Habilitamos CORS
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hola Grupo")
+});
+
+app.listen(PORT, (err)=>{
+    console.log(
+        err
+        ? "Se produjo un error al iniciar el servidor"
+        : `Servidor levantado en http://localhost:${PORT}`
+    );
+});
