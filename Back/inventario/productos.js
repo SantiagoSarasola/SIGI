82ead1dba5acf419b448f8b6edc7e57e8c520273
Martin Zalazar <martinzetav@gmail.com>
2024-10-31 12:01:55 -0300

    se creo el endpoint para POST

--- Back/inventario/productos.js
+++ Back/inventario/productos.js
@@ -1,6 +1,20 @@
 import express from "express";
-// import { db } from "../db.js";
+import { db } from "../db.js";
 
 const router = express.Router();
+
+router.post("/", async (req, res)=>{
+    const {nombre_producto, stock_actual, precio_lista, descuento_uno, costo_intermedio, descuento_dos, costo_final, incremento, precio_sugerido, precio_final, ganancia, id_categoria, id_fabrica} = req.body;
+
+    try {
+        await db.execute( `CALL spNuevoProducto (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [nombre_producto, stock_actual, precio_lista, descuento_uno, costo_intermedio, descuento_dos, costo_final, incremento, precio_sugerido, precio_final, ganancia, id_categoria, id_fabrica] );
+
+        return res.status(201).json({producto: {nombre_producto}});
+    } catch (error) {
+        console.error("Error al insertar el producto: ", error.message);
+        return res.status(500).json({ error: "Error al insertar el producto" });
+    }
+
+})
 
 export default router;
