97e2c65b34d26d0f1a2a2d2eb16a843f743583fe
Juan Robledo <juannrob@gmail.com>
2024-10-31 19:29:10 -0300

    Crear endpoint Get Productos

--- Back/inventario/productos.js
+++ Back/inventario/productos.js
@@ -1,6 +1,20 @@
 import express from "express";
-// import { db } from "../db.js";
+import { db } from "../db.js";
 
 const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const {
+    offset = 0,
+    limit = 10,
+    sort = "nombre_producto",
+    order = "ASC",
+  } = req.query;
+
+  const sql = "CALL get_productos(?, ?, ?, ?)";
+  const [productos] = await db.execute(sql, [offset, limit, sort, order]);
+
+  res.send({ productos });
+});
 
 export default router;
