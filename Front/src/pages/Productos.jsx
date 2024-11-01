d302cfd2ac320ec2589e35a281b9a3a02b5b2a40
Juan Robledo <juannrob@gmail.com>
2024-10-31 23:00:57 -0300

    Implentar funcionalidad de ordenamiento

--- Front/src/pages/Productos.jsx
+++ Front/src/pages/Productos.jsx
@@ -1,26 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import '../styles/Productos.css';
-import Menu from '../components/Menu';
+import React, { useState, useEffect } from "react";
+import "../styles/Productos.css";
+import Menu from "../components/Menu";
 
 function Productos() {
-  const [productos, setProductos] = useState([])
+  const [productos, setProductos] = useState([]);
+  const [sort, setSort] = useState("nombre_producto");
+
+  console.log(sort);
 
   useEffect(() => {
-    const traerProductos = async() => {
+    const traerProductos = async () => {
       try {
-        const resultado = await fetch("http://localhost:3000/productos")
-        const data = await resultado.json()
-        setProductos(data.productos[0])
-        console.log(data.productos[0])
+        const resultado = await fetch(
+          `http://localhost:3000/productos?offset=0&limit=10&sort=${sort}&order=DESC`
+        );
+        const data = await resultado.json();
+        setProductos(data.productos[0]);
       } catch (error) {
-        alert("no funca")
+        alert("no funca");
       }
-    }
-    
-    traerProductos()
-    
-  }, [])
-  
+    };
+
+    traerProductos();
+  }, [sort]);
 
   const handleVerDetalles = (id) => {
     alert(`voy a la ventana para ver los detalles del producto: ${id}`);
@@ -39,39 +41,50 @@ function Productos() {
       <div>
         <Menu />
       </div>
-    <div className="productos">
+      <div className="productos">
         <div className="header-productos">
           <h2>Productos</h2>
-          <button className="btn-nuevo" onClick={() => handleAgregar()}>Añadir Nuevo</button>
+          <button className="btn-nuevo" onClick={() => handleAgregar()}>
+            Añadir Nuevo
+          </button>
         </div>
-      <table className="productos-tabla">
-        <thead>
-          <tr>
-            <th>Buscar</th>
-            <th>ID Prod.</th>
-            <th>Nombre Producto</th>
-            <th>Precio de Lista</th>
-            <th>Precio de Venta</th>
-            <th>Stock</th>
-          </tr>
-        </thead>
-        <tbody>
-          {productos && productos.map((producto) => (
-            <tr key={producto.id_producto}>
-              <td>
-                <button onClick={() => handleVerDetalles(producto.id_producto)}>🔍</button>
-                <button onClick={() => handleBorrar(producto.id_producto)}>🗑️</button>
-              </td>
-              <td>{producto.id_producto}</td>
-              <td>{producto.nombre_producto}</td>
-              <td>${producto.precio_lista}</td>
-              <td>${producto.precio_final}</td>
-              <td>{producto.stock_actual}</td>
+        <table className="productos-tabla">
+          <thead>
+            <tr>
+              <th>Buscar</th>
+              <th>ID Prod.</th>
+              <th onClick={() => setSort("nombre_producto")}>
+                Nombre Producto
+              </th>
+              <th onClick={() => setSort("precio_lista")}>Precio de Lista</th>
+              <th>Precio de Venta</th>
+              <th>Stock</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {productos &&
+              productos.map((producto) => (
+                <tr key={producto.id_producto}>
+                  <td>
+                    <button
+                      onClick={() => handleVerDetalles(producto.id_producto)}
+                    >
+                      🔍
+                    </button>
+                    <button onClick={() => handleBorrar(producto.id_producto)}>
+                      🗑️
+                    </button>
+                  </td>
+                  <td>{producto.id_producto}</td>
+                  <td>{producto.nombre_producto}</td>
+                  <td>${producto.precio_lista}</td>
+                  <td>${producto.precio_final}</td>
+                  <td>{producto.stock_actual}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
