fb4cbe17ea3102f3e1ad4e29fd9da80fa4830e73
Juan Robledo <juannrob@gmail.com>
2024-11-02 18:24:50 -0300

    Implementar express-validator y validar paginación de GET: Productos

---
+++ Back/middlewares/validarPaginacionProductos.js
@@ -0,0 +1,29 @@
+import { query } from "express-validator";
+
+const validarPaginacionProductos = () => [
+  query("offset")
+    .optional()
+    .isInt({ min: 0 })
+    .withMessage("El offset debe ser un número entero mayor o igual a 0.")
+    .toInt(),
+
+  query("limit")
+    .optional()
+    .isInt({ min: 1, max: 100 })
+    .withMessage("El limit debe ser un número entero entre 1 y 100.")
+    .toInt(),
+
+  query("sort")
+    .optional()
+    .isString()
+    .isIn(["nombre_producto", "precio_lista", "precio_final", "stock_actual"])
+    .withMessage("El sort debe ser un campo válido para ordenar."),
+
+  query("order")
+    .optional()
+    .isString()
+    .isIn(["asc", "desc", "ASC", "DESC"])
+    .withMessage("El order debe ser 'asc', 'desc', 'ASC' o 'DESC'."),
+];
+
+export default validarPaginacionProductos;
