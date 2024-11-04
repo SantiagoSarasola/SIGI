f4e2761a96662023df820e08f9d36d5aea29d781
Agustin Zalazar <agustin.zalazar12@gmail.com>
2024-11-03 22:10:11 -0300

    Se hicieron cambios en las paginas de producto

--- Front/src/pages/Productos.jsx
+++ Front/src/pages/Productos.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../styles/Productos.css";
 import Menu from "../components/Menu";
-import { useNavigate } from 'react-router-dom';
+
 
 
 function Productos() {
