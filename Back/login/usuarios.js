7f61ab5c4c14ef4c01be5e7123ace5d8783ea4a7
SantiagoSarasola <santisarasola@yahoo.com.ar>
2024-11-05 22:58:40 -0300

    agregar DELETE para usuarios

--- Back/login/usuarios.js
+++ Back/login/usuarios.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { db } from "../db.js";
 import validarAtributosUsuario from "../middlewares/validarAtributosUsuario.js";
+import validarId from "../middlewares/validarId.js";
 import {validationResult} from "express-validator";
 import bcrypt from "bcrypt";
 
@@ -30,8 +31,6 @@ router.post("/", validarAtributosUsuario(), async (req, res) => {
     const idRol = req.body.idRol;
 
     const passwordHashed = await bcrypt.hash(password, 10);
-    // console.log(passwordHashed);
-    console.log(passwordHashed.length);
     try{
         const sql = "CALL spNuevoUsuario(?,?,?)";
         await db.execute(sql, [email, passwordHashed, idRol]);
@@ -40,6 +39,27 @@ router.post("/", validarAtributosUsuario(), async (req, res) => {
     } catch (error){
         return res.status(500).send({ error: "Error al crear usuario"});
     };
-})
+});
+
+router.delete("/:id", validarId(), async(req,res) => {
+  
+    const validacion = validationResult(req);
+    if (!validacion.isEmpty()) {
+      res.status(400).send({ errores: validacion.array() });
+      return;
+    }
+  
+    const id = Number(req.params.id);
+  
+    const sql = "CALL spEliminarUsuario(?)";
+  
+    try {
+      await db.query(sql, [id]);
+      return res.status(200).send({ id });
+    } catch (error) {
+      console.error("Error al eliminar el usuario: ", error.message);
+      return res.status(500).send({ error: "Error al eliminar el usuario" });
+    }
+  });
 
 export default router;
