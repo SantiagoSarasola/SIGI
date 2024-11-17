58ae0754008a1cfcb74e564f08924efba634d1a6
Luis <depetrisluis@gmail.com>
2024-11-17 10:03:35 -0300

    Agrego estructura de ventas y menu

--- Front/src/pages/Ventas.jsx
+++ Front/src/pages/Ventas.jsx
@@ -3,21 +3,95 @@ import Menu from '../components/Menu';
 import '../styles/Ventas.css';
 
 function Ventas() {
+
+  const handleAgregar = () => {
+    alert("Agregar una venta nueva");
+  };
+
   return (
     <>
-    <div>
+      <div>
         <Menu />
-    </div>
-    
-    
-    <div className="ventas">
-      
-      <div className="contenido">
-        <h2>Ventas</h2>
-        <p>ventana de Ventas.</p>
       </div>
-    </div>
+      <div className="ventas">
+        <div className="header-ventas">
+          <h2>Ventas</h2>
+          <button className="btn-nuevo" onClick={handleAgregar}>
+            Añadir Venta
+          </button>
+        </div>
 
+        
+        {/* <Paginacion
+          paginaActual={paginaActual}
+          totalPaginas={totalPaginas}
+          onPaginaChange={(nuevaPagina) => setPaginaActual(nuevaPagina)}
+          registrosVisibles={`Registros ${registrosInicio}-${registrosFin} de ${totalProductos}`}
+        /> */}
+
+        <table className="ventas-tabla">
+          <thead>
+            <tr>
+              <th></th>
+              <th>ID Venta.</th>
+              <th>
+                Fecha
+              </th>
+              <th>
+                Cantidad de Productos
+              </th>
+              <th>
+                Total Venta
+              </th>
+              <th>
+                Forma de Pago
+              </th>
+            </tr>
+            <tr>
+              <th></th>
+              <th></th>
+              <th>
+                {/* <input
+                  className="textBox"
+                  type="text"
+                  value={terminoBusqueda}
+                  onChange={(e) => setTerminoBusqueda(e.target.value)}
+                  placeholder="Buscar por nombre"
+                /> */}
+              </th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* {productos.map((producto) => (
+              <tr key={producto.id_producto}>
+                <td>
+                  <button
+                    className="btn-detalles"
+                    onClick={() => handleVerDetalles(producto.id_producto)}
+                  >
+                    🔍
+                  </button>
+                  <button
+                    className="btn-eliminar"
+                    onClick={() => handleBorrar(producto.id_producto)}
+                  >
+                    🗑️
+                  </button>
+                </td>
+                <td>{producto.id_producto}</td>
+                <td>{producto.nombre_producto}</td>
+                <td>${producto.precio_lista}</td>
+                <td>${producto.precio_final}</td>
+                <td>{producto.stock_actual}</td>
+                <td>{producto.categoria}</td>
+              </tr>
+            ))} */}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
