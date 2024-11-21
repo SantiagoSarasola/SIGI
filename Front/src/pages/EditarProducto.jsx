dd67ed8c08090c39d8513d51f552bf73b9e84e18
Agustin Zalazar <agustin.zalazar12@gmail.com>
2024-11-21 16:50:01 -0300

    Se agregaron  estilos en Gestion Categorias

--- Front/src/pages/EditarProducto.jsx
+++ Front/src/pages/EditarProducto.jsx
@@ -29,9 +29,12 @@ const EditarProducto = () => {
   const handleGuardar = (productoActualizado) => {
     if(window.confirm("Esta seguro que desea editar este producto?")){
       alert("Producto actualizado")
+      console.log("Producto actualizado:", productoActualizado);
+      navigate(`/Productos`);
+    }else{
+      alert("Se cancelo la operacion!");
     }
-    console.log("Producto actualizado:", productoActualizado);
-    navigate(`/Productos`);
+    
   };
 
   const handleCancelar = () => {
